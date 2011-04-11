29e21ca8a9f2e7423c27d8dde442befda2396673
Luke Amdor <luke.amdor@t8webware.com>
2011-04-11 14:38:00 -0500

    Created a protocol module for the messages going over the wire

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,11 +1,13 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends ParentProject(info) {
-  lazy val web     = project("web", "Web", new WebProject(_)) 
-  lazy val central = project("central", "Central", new CentralProject(_)) 
-  lazy val api     = project("api", "API", new ApiProject(_))
+  lazy val web      = project("web", "Web", new WebProject(_)) 
+  lazy val protocol = project("protocol", "Protocol", new ProtocolProject(_)) 
+  lazy val central  = project("central", "Central", new CentralProject(_)) 
+  lazy val api      = project("api", "API", new ApiProject(_))
 
   class WebProject(info: ProjectInfo) extends DefaultWebProject(info)
+  class ProtocolProject(info: ProjectInfo) extends DefaultProject(info)
   class CentralProject(info: ProjectInfo) extends DefaultProject(info)
   class ApiProject(info: ProjectInfo) extends DefaultProject(info)
 }
