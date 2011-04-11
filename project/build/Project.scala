516eb56880ae1f730ddfd7deb798d87634ee6a8d
Luke Amdor <luke.amdor@t8webware.com>
2011-04-11 14:04:29 -0500

    Set up project with subprojects

---
+++ project/build/Project.scala
@@ -0,0 +1,12 @@
+import sbt._
+
+class Project(info: ProjectInfo) extends ParentProject(info) {
+  lazy val web     = project("web", "Web", new WebProject(_)) 
+  lazy val central = project("central", "Central", new CentralProject(_)) 
+  lazy val api     = project("api", "API", new ApiProject(_))
+
+  class WebProject(info: ProjectInfo) extends DefaultWebProject(info)
+  class CentralProject(info: ProjectInfo) extends DefaultProject(info)
+  class ApiProject(info: ProjectInfo) extends DefaultProject(info)
+}
+
