489d4b41553c1c39b66f9c41093eac77bce2f375
zcox <zcox522@gmail.com>
2011-04-11 14:40:14 -0500

    Added Lift

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,7 +5,18 @@ class Project(info: ProjectInfo) extends ParentProject(info) {
   lazy val central = project("central", "Central", new CentralProject(_)) 
   lazy val api     = project("api", "API", new ApiProject(_))
 
-  class WebProject(info: ProjectInfo) extends DefaultWebProject(info)
+  class WebProject(info: ProjectInfo) extends DefaultWebProject(info) {
+    val liftVersion = "2.3"
+    override def libraryDependencies = Set(
+      "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
+      "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
+      "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
+      "junit" % "junit" % "4.5" % "test",
+      "ch.qos.logback" % "logback-classic" % "0.9.26",
+      "org.scala-tools.testing" %% "specs" % "1.6.6" % "test",
+      "com.h2database" % "h2" % "1.2.138"
+    ) ++ super.libraryDependencies
+  }
   class CentralProject(info: ProjectInfo) extends DefaultProject(info)
   class ApiProject(info: ProjectInfo) extends DefaultProject(info)
 }
